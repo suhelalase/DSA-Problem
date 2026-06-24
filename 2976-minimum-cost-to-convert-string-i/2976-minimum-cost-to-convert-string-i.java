2de7ee0c67165395863c4aebd0aa42eb502564ce
Suhel <suhelalase3@gmail.com>
2026-06-24 16:06:49 +0530

    Time: 21 ms (74.3%), Space: 47.8 MB (79.88%) - LeetHub

---
+++ 2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.java
@@ -0,0 +1,49 @@
+class Solution {
+    public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
+        
+        int n = 26;
+        long INF = (long) 1e15;
+
+        long[][] dist = new long[n][n];
+
+        // Step 1: initialize
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) dist[i][j] = 0;
+                else dist[i][j] = INF;
+            }
+        }
+
+        // Step 2: add edges
+        for (int i = 0; i < original.length; i++) {
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            dist[u][v] = Math.min(dist[u][v], cost[i]);
+        }
+
+        // Step 3: Floyd Warshall
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != INF && dist[k][j] != INF) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        // Step 4: compute answer
+        long total = 0;
+
+        for (int i = 0; i < source.length(); i++) {
+            int s = source.charAt(i) - 'a';
+            int t = target.charAt(i) - 'a';
+
+            if (dist[s][t] == INF) return -1;
+
+            total += dist[s][t];
+        }
+
+        return total;
+    }
+}
